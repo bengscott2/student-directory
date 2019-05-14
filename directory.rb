0655651b4b21daf291f8271e645eeebd2267a747
Ben Scott <bengscott2@gmail.com>
2019-05-14 11:54:22 +0100

    Refactoring after running program through linter(Rubocop)

--- directory.rb
+++ directory.rb
@@ -1,15 +1,17 @@
+# frozen_string_literal: true
+
 require 'csv'
 
 @students = []
 
 def input_students
-  puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
+  puts 'Please enter the names of the students'
+  puts 'To finish, just hit return twice'
   add_student_loop(STDIN.gets.chomp)
 end
 
 def add_student(name, cohort)
-  @students << {name: name, cohort: cohort.to_sym}
+  @students << { name: name, cohort: cohort.to_sym }
 end
 
 def interactive_menu_selector_loop
@@ -20,34 +22,27 @@ def interactive_menu_selector_loop
 end
 
 def print_menu
-  puts "1. Input the students"
-  puts "2. Show the students"
-  puts "3. Save list of students"
-  puts "4. Load list of students"
-  puts "9. Exit" # No. 9 because we'll be adding more options
+  puts '1. Input the students'
+  puts '2. Show the students'
+  puts '3. Save list of students'
+  puts '4. Load list of students'
+  puts '9. Exit' # No. 9 because we'll be adding more options
 end
 
 def process_selection(selection)
   case selection
-  when "1"
-    students = input_students
-  when "2"
-    show_students
-  when "3"
-    save_students(ask_for_filename)
-  when "4"
-    warning
-    load_students(ask_for_filename)
-  when "9"
-    exit
-  else
-    puts "I don't know what you mean try again."
+  when '1'then input_students
+  when '2'then show_students
+  when '3'then save_students(ask_for_filename)
+  when '4'then warning; load_students(ask_for_filename)
+  when '9'then exit
+  else puts "I don't know what you mean try again."
   end
 end
 
 def print_header
-  puts "The students of Villains Academy"
-  puts "-------------"
+  puts 'The students of Villains Academy'
+  puts '-------------'
 end
 
 def print_students_list
@@ -73,11 +68,11 @@ def save_students(filename)
       csv << student_data
     end
   end
-  puts "File succesfully saved."
+  puts 'File succesfully saved.'
 end
 
 def ask_for_filename
-  puts "Please enter the filename you would like to use.."
+  puts 'Please enter the filename you would like to use..'
   filename = STDIN.gets.chomp
   filename
 end
@@ -89,51 +84,46 @@ def load_students(filename)
     add_student(name, cohort)
   end
 
-  puts "File successfully loaded."
+  puts 'File successfully loaded.'
 end
 
 def get_cohort
-  months = ["January", "February", "March", "April", "May", "June", "July",
-     "August", "September", "October", "November", "December"]
-  loop do
-    puts "Please enter the cohort for the student"
-    cohort = STDIN.gets.chomp
-    if months.include?(cohort)
-      return cohort.to_s
-    else
-      puts "You've entered an invalid month, please try again"
-    end
+  months = %w[january february march april may june july august september
+              october november december]
+  puts 'Please enter the cohort for the student'
+  cohort = STDIN.gets.chomp
+  if months.include?(cohort.downcase)
+    return cohort.to_s.capitalize
+  else
+    puts "You've entered an invalid month, please try again"
+    get_cohort
   end
 end
 
 def add_student_loop(name)
   # while the name is not empty, repeat this code
-  while !name.empty? do
+  until name.empty?
     # add the student hash to the array
     add_student(name, get_cohort)
     puts "Now we have #{@students.count} students"
-    puts "Please enter the name of the next student."
+    puts 'Please enter the name of the next student.'
     name = STDIN.gets.chomp
   end
 end
 
 def warning
-  puts "Warning: If you proceed you will lose your current working directory"
+  puts 'Warning: If you proceed you will lose your current working directory'
   puts "Please type 'yes' to proceed"
   input = STDIN.gets.chomp
-  if input == "yes"
-    return
-  else
-    exit
-  end
+  exit if input != 'yes'
 end
 
 def try_load_students
   filename = ARGV.first # first argument from the command line
   if filename.nil? # get out of the method if it isn't given
-    puts "Loading files from students.csv"
-    load_students("students.csv")
-  elsif File.exists?(filename) # if it exists
+    puts 'Loading files from students.csv'
+    load_students('students.csv')
+  elsif File.exist?(filename) # if it exists
     load_students(filename)
     puts "Loaded #{@students.count} from #{filename}"
   else # if it doesn't exist
@@ -141,6 +131,7 @@ def try_load_students
     exit # quit the program
   end
 end
+
 
 try_load_students
 interactive_menu_selector_loop
