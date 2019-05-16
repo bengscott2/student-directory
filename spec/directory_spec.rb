c0a08e1c57c7527b1733d4c928af5734bd74457c
Alexis Josephides <ajosephides@gmail.com>
2019-05-16 11:47:48 +0100

    added test to verify that method ask for filename returns stdin

--- spec/directory_spec.rb
+++ spec/directory_spec.rb
@@ -20,3 +20,14 @@ describe 'print_footer' do
     expect { print_footer }.to output("Overall, we have 2 great students\n").to_stdout
   end
 end
+
+describe 'ask_for_filename' do
+  context "user enters a filename" do
+    let (:user_input) { "students.csv\n" }
+
+    it "returns the user input as a filenam" do
+      allow(STDIN).to receive(:gets) {user_input}
+      expect(ask_for_filename).to eq("students.csv")
+    end
+  end
+end
