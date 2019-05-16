725b30333f68edbd74e0c976e508338c899919e9
Alexis Josephides <ajosephides@gmail.com>
2019-05-16 11:18:02 +0100

    added test to check that a menu selection defaults to sorry message

--- spec/directory_spec.rb
+++ spec/directory_spec.rb
@@ -5,3 +5,9 @@ describe 'print_header' do
   expect { print_header }.to output("The students of Villains Academy\n-------------\n").to_stdout
   end
 end
+
+describe 'process_selection' do
+  it 'will print a sorry message when a not valid switch input is passed' do
+    expect {process_selection("10")}.to output("I don't know what you mean try again.\n").to_stdout
+  end
+end
